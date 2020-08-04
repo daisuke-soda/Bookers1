5637b245777abeb82aa8e4e3e84238ae45250832
daisuke-soda <sdobsession23@gmail.com>
2020-08-04 16:46:58 +0900

    [Finish]

---
+++ db/migrate/20200724100530_create_bookgs.rb
@@ -0,0 +1,10 @@
+class CreateBookgs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookgs do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
